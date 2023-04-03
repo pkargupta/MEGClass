b0107c717be9ff9a3cce686d373e0c9e0cf19f9a
Priyanka Kargupta <priyankakargupta@gmail.com>
2023-04-03 09:51:33 -0500

    updated class-oriented code

--- classKeyWords/run.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-gpu=$1
-dataset=$2
-vocab=$3
-outfile=$4
-
-CUDA_VISIBLE_DEVICES=${gpu} python static_repr.py --dataset_name ${dataset} --vocab_file ${vocab}
-CUDA_VISIBLE_DEVICES=${gpu} python gen_class_keywords.py --dataset_name ${dataset} --vocab_file ${vocab} --out_file ${outfile}
